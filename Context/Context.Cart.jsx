f1268f5984896ede40305c38e3f0f94837aeba14
AxelPalHu <155117078+AxelPalHu@users.noreply.github.com>
2024-03-18 06:28:03 -0500

    Add files via upload

---
+++ Context/Context.Cart.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+
+export const ContextCart = createContext();
+
+export const ProviderContext = ({ children }) => {
+    const [cartShopping, setCartShopping] = useState([]);
+
+    const addProductCart = (product) => {
+        const existProduct = cartShopping.find((item) => item.id === product.id);
+        if (existProduct) {
+            const newProduct = {
+                ...product,
+                quantity: existProduct.quantity + 1
+            };
+            const updateCarrito = cartShopping.map((item) => (
+                item.id === existProduct.id ? newProduct : item
+            ));
+            setCartShopping(updateCarrito);
+        } else {
+            const newProduct = {
+                ...product,
+                quantity: 1
+            };
+            setCartShopping([...cartShopping, newProduct]);
+        }
+    };
+
+    return (
+        <ContextCart.Provider value={{ addProductCart, cartShopping }}>
+            {children}
+        </ContextCart.Provider>
+    );
+};
